f4d4c0afa69bc81dbc7ec96a1ecb8e64db2f26d8
dharanshetty05 <dharanshetty05@gmail.com>
2025-08-30 11:25:27 +0530

    Routing fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="bg-white shadow p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold text-blue-600">DHARAN SHETTY</h1>
       <ul className="flex space-x-4">
-        <li><Link to="/" className="text-gray-700 hover:text-blue-600">Home</Link></li>
+        <li><Link to="/MyPortfolio" className="text-gray-700 hover:text-blue-600">Home</Link></li>
         <li><Link to="/about" className="text-gray-700 hover:text-blue-600">About</Link></li>
         <li><Link to="/projects" className="text-gray-700 hover:text-blue-600">Projects</Link></li>
         <li><Link to="/skills" className="text-gray-700 hover:text-blue-600">Skills</Link></li>
